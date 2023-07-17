2ca9b5f074b75c3a9ca9764918aa27668cfb0e8a
Rudy Sarmiento <rudy750@github.com>
2023-07-17 17:57:10 -0400

    initial commit

---
+++ cdk/lib/gh-webhook-stack.ts
@@ -0,0 +1,32 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as api from 'aws-cdk-lib/aws-apigateway';
+
+export default class WebHook extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    // The code that defines your stack goes here
+    // eslint-disable-next-line no-unused-vars
+    const githubWebhook = new lambda.NodejsFunction(this, 'githubWebhook', {
+      memorySize: 1024,
+      entry: '././src/webhook-lambda/src/index.js',
+      timeout: Duration.minutes(5),
+      environment: {
+       
+      }
+    });
+
+    // eslint-disable-next-line no-unused-vars
+    const apiWebhook = new api.RestApi(this, 'github-app-webhook-api', {
+      description: 'GitHub enpoint for webhook events',
+      deployOptions: {
+        stageName: 'production'
+      }
+    });
+
+    apiWebhook.root
+      .addResource('gh-webhook')
+      .addMethod('POST', new api.LambdaIntegration(githubWebhook));
+  }
+}

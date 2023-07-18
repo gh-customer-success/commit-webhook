1befa6d6335e2f1da9a309a9701ff4ce6b323b3d
Rudy Sarmiento <rudy750@github.com>
2023-07-17 20:00:36 -0400

    change to us east 2

--- bin/gh-webhook.ts
+++ bin/gh-webhook.ts
@@ -15,7 +15,7 @@ new GhWebhookStack(app, 'GhWebhookStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '066866130858', region: 'us-east-1' },
+  // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
